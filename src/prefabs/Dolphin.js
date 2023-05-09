396adbb92197ea04d5ebaf9f86fd219f7a1067e6
Danny C <dchan27@ucsc.edu>
2023-05-08 17:33:38 -0700

    cant figure out how to make shark work

---
+++ src/prefabs/Dolphin.js
@@ -0,0 +1,9 @@
+class Dolphin extends Phaser.GameObjects.Sprite
+{
+    constructor(scene,x,y,texture,frame)
+    {
+        super(scene,x,y,texture,frame);
+        scene.add.existing(this);
+    }
+
+}
---
+++ src/prefabs/Shark.js
@@ -0,0 +1,8 @@
+class Shark extends Phaser.GameObjects.Sprite
+{
+    constructor(scene,x,y,texture,frame)
+    {
+        super(scene,x,y,texture,frame);
+        scene.add.existing(this);
+    }
+}
