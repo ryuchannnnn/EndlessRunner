8e67d852de5fbb5c7afddccbe330f51fe2d223b1
Danny C <dchan27@ucsc.edu>
2023-05-09 03:29:41 -0700

    i found out that I have to make my own sprites

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -1,12 +1,1 @@
-// background
-// https://opengameart.org/content/underwater-scene-loopable
-
-// sprites
-// https://rapidpunches.itch.io/random-ocean-creatures
-
-// coin
-// https://opengameart.org/content/coin-2
-
-// shark sprite 
-// https://sergeant-slash.itch.io/32x32-animated-shark-sprite
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,18 +7,12 @@ class Play extends Phaser.Scene
 
     preload()
     {
-        this.load.image('underWaterBackground', './assets/background.png');
-        this.load.image('dolphin', './assets/dolphin.png');
-        // this.load.image('shark', './assets/shark.png', {frameWidth:64, frameHeight:32, startFrame:0, endFrame:7});
+
     }
 
     create()
     {
-        // backgroun image 
-        this.underWaterBackground = this.add.tileSprite(0,0,840,480,'underWaterBackground').setOrigin(0,0);
-        
-        // moveable dolphin 
-        this.p1Dolphin = new Dolphin(this,game.config.width/2, game.config.height - borderUISize - borderPadding, 'dolphin').setOrigin(0.5,0);
+
         
         // keyboard keys for moving 
         keyW = this.input.keyboard.addkey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -42,6 +36,6 @@ class Play extends Phaser.Scene
 
     update()
     {
-        this.underWaterBackground.tilePositionX -= 4.5;
+
     }
 }
