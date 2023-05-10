0c0a85df171fb9930659e9edf11f0a660d73cb4a
Danny C <dchan27@ucsc.edu>
2023-05-09 20:09:43 -0700

    not sure what is currently wrong with animation

---
+++ src/prefabs/Submarine.js
@@ -0,0 +1,9 @@
+class Submarine extends Phaser.GameObjects.Sprite
+{
+    constructor(scene,x,y,texture,frame)
+    {
+        super(scene,x,y,texture,frame);
+
+        scene.add.existing(this);
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,22 @@ class Play extends Phaser.Scene
     preload()
     {
         this.load.image('waterBackground', './assets/waterBackground.png');
+
+        this.load.atlas('submarine', './assets/submarineSprSheet.png', './assets/submarine.json');
+        this.anims.create({
+                key:"subMove",
+                frameRate: 6,
+                frames: this.anims.generateFrameNames("submarine", 
+                {
+                    prefix: "submarine0",
+                    suffix: ".png",
+                    start: 0,
+                    end: 6
+                }
+                ),
+                repeat:-1
+            }
+        );
     }
 
     create()
@@ -29,6 +45,11 @@ class Play extends Phaser.Scene
         keyI = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
         keyB = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
         
+        this.p1Submarine = new Submarine(this, 0, 30, 'submarine', 'submarine01').setOrigin(0,0);
+
+        let subAnim: Phaser.GameObjects.Sprite = this.add.sprite(0,30, "submarine", "submarine01.png");
+        subAnim.play("subMove");
+
         // tester
         // this.add.text(320,150, "Endless Runner Play");
     }
