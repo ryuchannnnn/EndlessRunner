e44c1142b4c87611c08c45fb52d0ecbc038f5d24
Danny C <dchan27@ucsc.edu>
2023-05-13 06:12:35 -0700

    fixing credit scene

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -13,14 +13,14 @@ class Credits extends Phaser.Scene
 
     update()
     {   
-        this.add.text((game.config.width/2) - 20, 50, 'Credits: ');
-        this.add.text((game.config.width/2) - 20, 100, 'BGM Music: https://opengameart.org/content/underwater-theme');
-        this.add.text((game.config.width/2) - 20, 100, 'Picking Up coin: https://opengameart.org/content/picked-coin-echo-2');
-        this.add.text((game.config.width/2) - 20, 150, 'Key press: https://opengameart.org/content/start-button');
+        this.add.text(0 , 50, 'Credits: ');
+        this.add.text(0 , 100, 'BGM Music: https://opengameart.org/content/underwater-theme');
+        this.add.text(0, 150, 'Picking Up coin: https://opengameart.org/content/picked-coin-echo-2');
+        this.add.text(0, 200, 'Key press: https://opengameart.org/content/start-button');
         // need a death sound maybe metal breaking or crash sound
 
 
-        this.add.text((game.config.width/2) - 20, 300, 'Press B to go back to menu');
+        this.add.text(50, 300, 'Press B to go back to menu');
 
         if(Phaser.Input.Keyboard.JustDown(keyB))
         {
