d1ca6423e3ed6f28052c77ddb1756e588d3947c8
Danny C <dchan27@ucsc.edu>
2023-05-13 06:20:33 -0700

    adding jurisdiction

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // Danny Chan
-// Run
-// hrs spent: 
+// Below the Marine
+// hrs spent: 30
 /*
 creative jurisdiction:
-
+    I learned how to make a playable sprite animated because I was not able to find a guide that was recent (<2 years) that showed me how to do it. 
+    My visual style is I tried to keep everything blue or underwater themed because the setting of the game takes place in the ocean. I think I am trying
+    something new with endless runner as most people are doing a survival, but I added on the aspect where your score is not dependent on survival time, but 
+    rather only collecting coins.
 */
 
 
